2daafc0340121eb8ae4c040821dd69b0003ae72d
Alireza Nazari <Alireza Nazari>
2017-03-29 16:05:52 -0400

    Add a new test

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,5 +1,8 @@
 angular.module('playlistModule')
 
+it('has a dummy spec to test 3 + 3', function() {
+    expect(3+3).toEqual(12);
+});
     .controller('BookController', function($scope, $routeParams) {
         $scope.name = 'BookController';
         $scope.params = $routeParams;
